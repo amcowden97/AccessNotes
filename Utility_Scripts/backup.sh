78f829b546baccf5185bff63e1b06905eea64116
amcowden97 <am.cowden.97@gmail.com>
2018-01-23 18:49:03 -0600

    Updated Structure to Include Automatic USB Device Prompting

--- Utility_Scripts/backup.sh
+++ Utility_Scripts/backup.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 
-#Error Constants
-INVAL_ARG=55
-INVAL_DIR=56
-
-#Error Function Handler
-function error_function {
-	case $1 in
-		"$INVAL_ARG")
-			echo "Invalid Number of Arguments... Usage $0 sourcedirectory destdirectory"
-		;;
-		"$INVAL_DIR")
-			echo "The Argument Given is not a Directory... Usage $0 sourcedirectory destdirectory"
-		;;
-	esac
-	
-	exit $1
-}
-
 #Argument Number Handler
-if [ $# -gt 2 ] || [ $# -lt 2 ]; then
-	error_function $INVAL_ARG
+if [ $# -ne 0 ]; then
+	echo "Usage: This Script Does Not Take Any Command Line Arguments"
+	exit 1
 fi
 
-#Directory Argument Handler
-if [ ! -d "$1" ] && [ ! -d "$2" ]; then
-	error_function $INVAL_DIR
-fi
-
-#Copying File
-for file in ${1}/*; do
-	file_name="$(basename "$file")".bak
-	echo "Writing " $file_name " to " $2
-done
+udevadm info --query=all --name=sdb #| grep -E "MODEL=|VENDOR=|PATH="
 
 exit 0
